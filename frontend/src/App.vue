f16f70577f12aaee1e9e48274f05fc5450d19fea
Thalida Noel <hello@thalida.com>
2024-03-24 21:43:28 +0000

    Setup loading state

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { onMounted, watchEffect } from 'vue';
 import { RouterView } from 'vue-router'
 import { useAuthStore } from './stores/auth'
 import { useUIStore } from './stores/ui'
 import { useCoreStore } from './stores/core'
-import { onMounted, watchEffect } from 'vue';
+import NapkinNotesLogo from './components/icons/NapkinNotesLogo.vue'
 
 const uiStore = useUIStore()
 const authStore = useAuthStore()
@@ -33,10 +34,41 @@ watchEffect(() => {
 
 <template>
   <RouterView v-if="!isLoading" />
-  <div v-else>
-    Loading...
+  <div v-else class="flex flex-col gap-4 items-center justify-center w-full h-full bg-gradient-to-br from-slate-300 via-blue-200 to-sky-20 dark:from-slate-900 dark:via-slate-900 dark:to-slate-950">
+    <NapkinNotesLogo class="animate-grow-spin relative inline-block w-16 h-16 rounded-lg drop-shadow-xl" />
   </div>
 </template>
 
 <style scoped>
+.animate-grow-spin {
+  -webkit-animation: grow 3s linear infinite;
+  animation: grow 3s linear infinite;
+}
+
+@keyframes grow {
+  0% {
+    transform: scale(1) rotate(0deg);
+  }
+  33% {
+    transform: scale(1.5) rotate(-30deg);
+  }
+  66% {
+    transform: scale(1.5) rotate(20deg);
+  }
+  100% {
+    transform: scale(1) rotate(0deg);
+  }
+}
+
+
+.animate-flipx {
+    -webkit-animation: flipX 2s linear infinite;
+    animation: flipX 2s linear infinite;
+}
+
+@keyframes flipX {
+  0%{ transform: perspective(200px) rotateX(0deg) rotateY(0deg); }
+  50%{ transform: perspective(200px) rotateX(-180deg) rotateY(0deg); }
+  100%{ transform: perspective(200px) rotateX(-180deg) rotateY(-180deg) }
+}
 </style>
