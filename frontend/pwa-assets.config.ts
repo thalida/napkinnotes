54032e2f2937cd733b74d4006eabe6722dd251d0
Thalida Noel <hello@thalida.com>
2024-03-25 00:14:29 +0000

    Updated icons

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
+import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset,
+  headLinkOptions: {
+    preset: "2023"
+  },
+  preset: {
+    ...minimal2023Preset,
+    maskable: {
+      sizes: [512],
+      padding: 0,
+      resizeOptions: {
+        background: { r: 0, g: 0, b: 0, alpha: 0 }
+      }
+    },
+    apple: {
+      sizes: [180],
+      resizeOptions: {
+        background: { r: 0, g: 0, b: 0, alpha: 0 }
+      }
+    }
+  },
   images: [
     'public/logo-white.svg',
   ]
