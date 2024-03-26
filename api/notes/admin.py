53ab97df1b0a6f6f26f70973e305fe9ca55c567e
Thalida Noel <hello@thalida.com>
2024-03-26 08:44:42 +0000

    Display if note is hidden on admin

--- api/notes/admin.py
+++ api/notes/admin.py
@@ -6,7 +6,7 @@ from .models import Note
 
 @admin.register(Note)
 class NoteAdmin(ModelAdmin):
-    list_display = ("user", "created_at", "updated_at")
+    list_display = ("user", "created_at", "updated_at", "is_hidden")
     search_fields = ["user__email", "content"]
     list_filter = ["user"]
     readonly_fields = ["user", "created_at", "updated_at"]
