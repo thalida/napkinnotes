d3b6ef4a652b5813dc4964192e0a3233fa5e9b7e
Thalida Noel <hello@thalida.com>
2024-03-31 15:13:19 +0000

    Disallow nested headers; Fix inner content;

--- app/src/lib/widgets/headings.ts
+++ app/src/lib/widgets/headings.ts
@@ -7,17 +7,26 @@ export default class HeadingsWidget extends Widget {
   insert(textNode: Text, text: string) {
     const headingLevel = text.match(/#/g)?.length || 0
     const heading = document.createElement(`h${headingLevel}`)
-    heading.textContent = text.replace(/#/g, '').trim() + ' '
+    heading.innerHTML = text.replace(/#/g, '').trim() + '&ZeroWidthSpace;'
     textNode.replaceWith(heading)
     setCursorInElement(heading)
   }
 
   onKeyup(): boolean {
-    const { focusedNode } = getNodesAtCursor()
+    const { focusedNode, cursorTarget } = getNodesAtCursor()
     const isInsertWidget = focusedNode
       ? this.SYNTAX_REGEX.test(focusedNode.textContent || '')
       : false
     if (!isInsertWidget) {
+      return false
+    }
+
+    const isHeading =
+      cursorTarget?.nodeName === 'H1' ||
+      cursorTarget?.nodeName === 'H2' ||
+      cursorTarget?.nodeName === 'H3' ||
+      cursorTarget?.nodeName === 'H4'
+    if (isHeading) {
       return false
     }
 
